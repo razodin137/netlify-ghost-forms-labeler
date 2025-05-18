001d4cda62d65854e23f89577412dfd67ae5bf4f
cam <72155693+razodin137@users.noreply.github.com>
2025-05-18 19:32:55 +0700

    Add files via upload

---
+++ netlify/functions/create-member.js
@@ -0,0 +1,43 @@
+const { AdminAPI } = require('@tryghost/admin-api');
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed'
+        };
+    }
+
+    const { firstName, lastName, email } = Object.fromEntries(new URLSearchParams(event.body));
+    if (!email) {
+        return {
+            statusCode: 400,
+            body: 'Email is required'
+        };
+    }
+
+    const api = new AdminAPI({
+        url: process.env.GHOST_ADMIN_URL,
+        key: process.env.GHOST_ADMIN_API_KEY,
+        version: 'v5.0'
+    });
+
+    try {
+        await api.members.add({
+            email,
+            name: `${firstName || ''} ${lastName || ''}`.trim(),
+            labels: ['Netlify']
+        });
+
+        return {
+            statusCode: 200,
+            body: 'Success'
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: `Error: ${error.message}`
+        };
+    }
+};
+
